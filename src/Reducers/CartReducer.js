42f6dd0bee71dd0358e4a3576a0e82c89e0eec9b
istiaq-tanim <istiaq.tanim17@gmail.com>
2025-10-06 13:17:24 +0600

    reducer cleaner

--- src/Reducers/CartReducer.js
+++ src/Reducers/CartReducer.js
@@ -14,48 +14,52 @@ function CartReducer(state, action) {
                               ],
                               products: state?.products?.map((item) => item.id === action.product.id ? { ...item, stock: item.stock - 1 } : item)
                         }
-                  } else {
-                        return {
-                              ...state,
-                              carts: state.carts.map((cart) => {
-                                    if (cart.id === action.product.id) {
-                                          return {
-                                                ...cart,
-                                                quantity: cart.quantity + 1
-                                          }
-                                    } else {
-                                          return cart
-                                    }
-                              }),
-                              products: state?.products?.map((item) => item.id === action.product.id ? { ...item, stock: item.stock - 1 } : item)
-                        }
                   }
+                  return {
+                        ...state,
+                        carts: state.carts.map((cart) => {
+                              if (cart.id === action.product.id) {
+                                    return {
+                                          ...cart,
+                                          quantity: cart.quantity + 1
+                                    }
+                              } else {
+                                    return cart
+                              }
+                        }),
+                        products: state?.products?.map((item) => item.id === action.product.id ? { ...item, stock: item.stock - 1 } : item)
+                  }
+
             }
             case "remove_cart": {
-                  const findIndex = state.carts.findIndex(item => item.id === action.id)
+                  const findItem = state.carts.find(item => item.id === action.id)
+
+                  if (!findItem) return state
                   return {
                         ...state,
                         carts: state.carts.filter(cart => cart.id !== action.id),
-                        products: state?.products?.map((item) => item.id === action.id ? { ...item, stock: item.stock + state.carts[findIndex].quantity } : item)
+                        products: state?.products?.map((item) => item.id === action.id ? { ...item, stock: item.stock + findItem.quantity } : item)
                   }
             }
 
             case "decrease_cart": {
-                  const findIndex = state.carts.findIndex(item => item.id === action.id)
+                  const findItem = state.carts.find(item => item.id === action.id)
 
-                  if (state.carts[findIndex].quantity > 1) {
+                  if (!findItem) return state
+
+                  if (findItem.quantity > 1) {
                         return {
                               ...state,
                               carts: state.carts.map((item) => item.id === action.id ? { ...item, quantity: item.quantity - 1 } : item),
                               products: state?.products?.map((item) => item.id === action.id ? { ...item, stock: item.stock + 1 } : item)
                         }
-                  } else {
-                        return {
-                              ...state,
-                              carts: state.carts.filter(cart => cart.id !== action.id),
-                              products: state?.products?.map((item) => item.id === action.id ? { ...item, stock: item.stock + state.carts[findIndex].quantity } : item)
-                        }
                   }
+                  return {
+                        ...state,
+                        carts: state.carts.filter(cart => cart.id !== action.id),
+                        products: state?.products?.map((item) => item.id === action.id ? { ...item, stock: item.stock + findItem.quantity } : item)
+                  }
+
             }
             default:
                   return state;
